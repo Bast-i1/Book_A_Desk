016da722c27481bcf3723d437f5f64bd0ec93a1a
Bastien Duban <duban.bastien@gmail.com>
2021-03-03 12:19:46 +0100

    href link from an office to the office show

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :offices, dependent: :destroy
+  has_many :meetingrooms, dependent: :destroy
   has_many :bookings
   validates :email, presence: true, uniqueness: true
 
